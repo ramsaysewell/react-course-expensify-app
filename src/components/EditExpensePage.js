046f971fc8d39ac7f2b09d521ca2d1dd93cd938a
Ramsay Sewell <ramsay@Ramsays-MacBook-Pro.local>
2020-09-06 11:52:43 +0100

    Setup startRemoveExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
-
-// should handle EditExpensePage
-// snapshot
-
-// should handle editExpense
-// spies
-
-// should handle removeExpense
-// spies
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
 	onSubmit = (expense) => {
@@ -18,7 +9,7 @@ export class EditExpensePage extends React.Component {
 		this.props.history.push('/');
 	};
 	onRemove = () => {
-		this.props.removeExpense({ id: this.props.expense.id });
+		this.props.startRemoveExpense({ id: this.props.expense.id });
 		this.props.history.push('/');
 	};
 	render() {
@@ -44,7 +35,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch, props) => ({
 	editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-	removeExpense: (data) => dispatch(removeExpense(data)),
+	startRemoveExpense: (data) => dispatch(startRemoveExpense(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { addExpense, removeExpense, editExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { addExpense, removeExpense, editExpense, startAddExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -27,6 +27,21 @@ test('should setup remove expense action object', () => {
 		id: '123abc',
 	});
 });
+
+test('should remove expense from firebase', (done) => {
+	const store = createMockStore();
+	store.dispatch(startRemoveExpense({ id: expenses[2].id })).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'REMOVE_EXPENSE',
+			id: expenses[2].id
+		});
+		return database.ref(`expenses/${expenses[2].id}`).once("value");
+	}).then((snapshot) => {
+		expect(snapshot.val()).toBeFalsy();
+		done();
+	})
+})
 
 test('should setup edit expense action object', () => {
 	const action = editExpense('123abc', { note: 'New note value' });
